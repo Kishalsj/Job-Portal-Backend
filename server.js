4dbae0260815413c144ec5f7c7e43c26b167e11c
KishalJayalath <kishalsj2002@gmail.com>
2025-03-11 21:43:09 +0530

    server update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import app from "./app.js";
 import cloudinary from "cloudinary";
+import { createServer } from "http";
 
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -12,6 +13,5 @@ app.get("/", (req, res) => {
   res.send("Your Backend is Running");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server listening at port ${process.env.PORT}`);
-});
+const server = createServer(app);
+export default server;
