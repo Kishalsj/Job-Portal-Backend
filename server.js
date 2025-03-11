2fd7592579302ce67caebd50b678178f088aeb84
KishalJayalath <kishalsj2002@gmail.com>
2025-03-11 21:16:25 +0530

    vercel file uploaded

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import express from "express";
 import app from "./app.js";
 import cloudinary from "cloudinary";
 
@@ -5,6 +6,10 @@ cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+app.get("/", (req, res) => {
+  res.send("Your Backend is Running");
 });
 
 app.listen(process.env.PORT, () => {
